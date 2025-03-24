f8474e895f363793b92dc8ad1695f941f1b4093c
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-23 21:29:12 -0300

    feat: adiciona a funcao de reiniciar a ferramenta

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
@@ -11,17 +10,56 @@ function sortear() {
         numeroAleatorio = gerarNumeroAleatorio(de, ate);
         
         while (numerosSorteados.includes(numeroAleatorio)) {
-            console.log('tenho que gerar de novo');            
             numeroAleatorio = gerarNumeroAleatorio(de, ate);
         }
 
         numerosSorteados.push(numeroAleatorio);
     }
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
-    console.log(numerosSorteados);
+
+    alterarTextoSorteio(numerosSorteados); 
+
+    alterarBotaoReiniciar();
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('texto-sorteio').innerText = `Números sorteados:  nenhum até agora`;
+
+    alterarBotaoSortear();
+
+    alterarBotaoReiniciar();
 }
 
 function gerarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function alterarBotaoReiniciar() {
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function alterarBotaoSortear() {
+    let botao = document.getElementById('btn-sortear');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function alterarTextoSorteio(valor) {
+    document.getElementById('texto-sorteio').innerText = `Números sorteados: ${valor}`;
 }
